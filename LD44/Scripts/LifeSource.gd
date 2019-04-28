5298f8dd645faa18818f7ed04994687e5f28968c
nacho <nama1510@gmail.com>
2019-04-28 21:01:28 +0200

    Plants now have Sprites and animations

--- LD44/Scripts/LifeSource.gd
+++ LD44/Scripts/LifeSource.gd
@@ -5,11 +5,17 @@ signal absorbed_life_updated(life_source_life)
 class_name life_source
 
 onready var collision_shape = get_node("CollisionShape2D")
-onready var lif_source_sprite = get_node("LifeSourceSprite")
+onready var life_source_sprite = get_node("LifeSourceSprite")
+onready var anim_player = get_node("AnimationPlayer")
 
 export var life_source_life : float = 20 
+func _ready():
+	anim_player.play("Idle_Alive")
+	
+	
 
 func kill_life_source():
-	lif_source_sprite.set_modulate(Color(1,0,0,1))
+	anim_player.play("Idle_Dead")
+
 	emit_signal("absorbed_life_updated",life_source_life)
 	collision_shape.disabled = true
