cee66ff5953e90f5562d3dcecde5f234f47f19c1
nacho <nama1510@gmail.com>
2019-04-29 17:01:11 +0200

    Now enemies notify when all of them are alive

--- LD44/Scripts/EnemiesInSector.gd
+++ LD44/Scripts/EnemiesInSector.gd
@@ -10,3 +10,9 @@ func _ready():
 
 func _on_new_enemy_alive():
 	print("new enemy alive")
+	for enemy in get_children():
+		if enemy != get_node("EnemyActivationArea"):
+			if enemy.is_alive == false:
+				return;
+	print("All enemies alive")
+	emit_signal("all_enemies_alive")
