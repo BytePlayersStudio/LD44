a4951c25cf99178a4776220b527f8ff215e454a9
Alejandro de Cea <alejandrodecea@gmail.com>
2019-04-30 02:35:54 +0200

    Added win message when game is beaten and fixed door positions

--- LD44/Scenes/GameManager.gd
+++ LD44/Scenes/GameManager.gd
@@ -1,4 +1,10 @@
 extends Node2D
 
-func _on_Player_kill_player():
+func reload():
 	get_tree().reload_current_scene()
+
+func _on_Player_kill_player():
+	reload()
+
+func _on_UI_on_game_beaten():
+	reload()
--- LD44/Scripts/UI/UI.gd
+++ LD44/Scripts/UI/UI.gd
@@ -1,11 +1,20 @@
 extends Control
 
+signal on_game_beaten
+
 onready var lives_label : Label = get_node("MarginContainer/HBoxContainer/LivesLabel")
+onready var win_container : MarginContainer = get_node("WinContainer")
+onready var timer : Timer = get_node("Timer")
 
 func _ready():
-	print(self)
 	Global.UI = self
-	print(Global.UI)
 
 func update_lives_label(absorbed_lives):
 	lives_label.set_text(str(absorbed_lives))
+
+func _on_all_enemies_alive():
+	win_container.set_visible(true)
+	timer.start()
+
+func _on_Timer_timeout():
+	emit_signal("on_game_beaten")
